6435b40d50698580ae285f7bea03dae092614a57
John Wass <jwass3@gmail.com>
2015-12-26 16:08:06 -0500

    update organization

--- project/Common.scala
+++ project/Common.scala
@@ -3,7 +3,7 @@ import sbt._
 
 object Common {
     val settings: Seq[Def.Setting[_]] = Seq(
-        organization := "wiii",
+        organization := "com.rxthings",
         licenses +=("Apache-2.0", url("http://www.apache.org/licenses/LICENSE-2.0")),
         resolvers += "jw3 at bintray" at "https://dl.bintray.com/jw3/maven",
         scalaVersion := "2.11.7",
