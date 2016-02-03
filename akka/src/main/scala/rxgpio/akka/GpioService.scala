a25bc99ef67c56942f2c57d5bc52b7dfe98d4a23
John Wass <jwass3@gmail.com>
2016-02-02 23:42:19 -0500

    track subscriptions

--- akka/src/main/scala/rxgpio/akka/GpioService.scala
+++ akka/src/main/scala/rxgpio/akka/GpioService.scala
@@ -1,10 +1,15 @@
 package rxgpio.akka
 
+import java.util.UUID
+
 import akka.actor._
 import com.typesafe.config.Config
 import gpio4s.gpiocfg.CfgIO._
+import rx.lang.scala.Subscription
 import rx.lang.scala.subjects.PublishSubject
 import rxgpio.akka.Device.{DeviceInstallFailed, DeviceInstalled, InstallDevice}
+
+import scala.collection.mutable
 
 
 /**
@@ -15,6 +20,7 @@ class GpioService(m: GpioInfo, pp: PinProducer = DefaultPinProducer) extends Act
 
     val gpios: PinAllocation = produceGpios(m, pp)
     val subscribers = PublishSubject[DigitalEvent]
+    val subscriptions = mutable.Map[String, Subscription]()
 
 
     //!\\ do not forward events - outsiders cannot touch PinRefs //!\\
@@ -22,7 +28,11 @@ class GpioService(m: GpioInfo, pp: PinProducer = DefaultPinProducer) extends Act
         case Configure(c) => configure(gpios, c)
         case Subscribe(p) =>
             val s = sender()
-            s ! subscribers.filter(_.pin == p).subscribe(s ! _)
+            val sub = subscribers.filter(_.pin == p).subscribe(s ! _)
+            val id = random()
+            subscriptions(id) = sub
+            s ! id
+
 
         case m @ DigitalRead(p) => gpios(p) ! m
         case m @ DigitalWrite(p, _) => gpios(p) ! m
@@ -55,4 +65,5 @@ object GpioService {
 
     def produceGpios(model: GpioInfo, pp: PinProducer)(implicit ctx: ActorContext): PinAllocation = model.pins.map(p => p -> pp.get(p)).toMap
     def configure(gpios: PinAllocation, conf: Config, reset: Boolean = false) = conf.pins().foreach(p => gpios(p.num) ! Setup(p))
+    def random() = UUID.randomUUID.toString
 }
