319ee1f4bdb5c0d5d9500469478c0d98ec230521
John Wass <jwass3@gmail.com>
2016-02-02 23:24:35 -0500

    update dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,23 +1,29 @@
 import sbt._
 
 object Dependencies {
-    val commonDependencies: Seq[ModuleID] = Seq(
-        "org.scala-lang" % "scala-library" % "2.11.7",
-        "org.scala-lang" % "scala-reflect" % "2.11.7",
+    val commonDependencies = {
+        val akkaVersion = "2.4.1"
+        val akkaStreamVersion = "2.0.1"
 
-        "wiii" %% "akka-injects" % "0.1",
-        "gpio4s" %% "gpiocfg" % "0.1",
-        "io.reactivex" %% "rxscala" % "0.25.0",
+        Seq(
+            "org.scala-lang" % "scala-library" % "2.11.7",
+            "org.scala-lang" % "scala-reflect" % "2.11.7",
 
-        "net.java.dev.jna" % "jna" % "4.2.1",
-        "com.nativelibs4java" % "jnaerator-runtime" % "0.12",
+            "wiii" %% "akka-injects" % "0.1",
+            "gpio4s" %% "gpiocfg" % "0.1",
+            "io.reactivex" %% "rxscala" % "0.25.0",
 
-        "com.typesafe.akka" %% "akka-actor" % "2.4.0",
-        "com.typesafe.akka" %% "akka-slf4j" % "2.4.0" % Runtime,
+            "net.java.dev.jna" % "jna" % "4.2.1",
+            "com.nativelibs4java" % "jnaerator-runtime" % "0.12",
 
-        "org.scalatest" %% "scalatest" % "2.2.5" % Test,
-        "com.typesafe.akka" %% "akka-testkit" % "2.4.0" % Test,
-        "org.scalamock" %% "scalamock-scalatest-support" % "3.2.2" % Test
-    )
+            "com.typesafe.akka" %% "akka-actor" % akkaVersion,
+            "com.typesafe.akka" %% "akka-slf4j" % akkaVersion % Runtime,
+            "com.typesafe.akka" %% "akka-stream-experimental" % akkaStreamVersion,
+
+            "org.scalatest" %% "scalatest" % "2.2.5" % Test,
+            "com.typesafe.akka" %% "akka-testkit" % "2.4.0" % Test,
+            "org.scalamock" %% "scalamock-scalatest-support" % "3.2.2" % Test
+        )
+    }
 }
 
